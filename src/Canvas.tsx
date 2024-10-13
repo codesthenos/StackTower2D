caa20247640f324d3785f453e83b43a619e480fb
codesthenos <codesthenos@gmail.com>
2024-10-13 03:16:22 +0200

    Incluido un efecto de confetti que solo triggea si ganamos, usando la libreria externa react-confetti, voy a mergear esta rama a la main

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -4,12 +4,24 @@ import { CANVAS_HEIGHT, CANVAS_WIDTH } from './constants.ts'
 import draw from './canvasDraw.ts'
 //Custom hook
 import useCanvas from './useCanvas.ts'
+//Confetti
+import Confetti from 'react-confetti'
+import { useEffect, useState } from 'react'
 
 function Canvas () {
   const { canvasRef, score, textEndGame } = useCanvas(draw)
+  const [showConfetti, setShowConfetti] = useState(false)
+  useEffect(() => {
+    if (textEndGame === 'YOU WIN') {
+      setShowConfetti(true)
+    } else {
+      setShowConfetti(false)
+    }
+  }, [textEndGame])
   
   return (
     <>
+      {showConfetti && <Confetti />}
       {score > 0 && <span>Score: {score}</span>}
       {textEndGame && <span>{textEndGame}</span>}
       <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>
